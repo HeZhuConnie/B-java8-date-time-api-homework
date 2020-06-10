830794c7967d197d111b3172037ef498a44149c0
he.zhu <he.zhu@thgoughtworks.com>
2020-06-10 22:51:49 +0800

    [Zhu He] complete practice 2

--- src/main/java/com/thoughtworks/capability/gtb/Practice2.java
+++ src/main/java/com/thoughtworks/capability/gtb/Practice2.java
@@ -1,16 +1,37 @@
 package com.thoughtworks.capability.gtb;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 /**
  * 对任意日期获取下一个工作日, 不考虑节假日
  *
  * @author itutry
- * @create 2020-05-15_17:20
+ * @author HeZhuConnie
  */
 public class Practice2 {
 
-  public static LocalDate getNextWorkDate(LocalDate date) {
-    return null;
-  }
+    public static LocalDate getNextWorkDate(LocalDate date) {
+        DayOfWeek dayOfWeek = date.getDayOfWeek();
+        LocalDate nextWorkDate;
+        switch (dayOfWeek) {
+            case SUNDAY:
+            case MONDAY:
+            case TUESDAY:
+            case WEDNESDAY:
+            case THURSDAY:
+                nextWorkDate = date.plusDays(1);
+                break;
+            case FRIDAY:
+                nextWorkDate = date.plusDays(3);
+                break;
+            case SATURDAY:
+                nextWorkDate = date.plusDays(2);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + dayOfWeek);
+        }
+
+        return nextWorkDate;
+    }
 }
