a59658f03367b3f53c03baaf91c8d697a80468a0
he.zhu <he.zhu@thgoughtworks.com>
2020-06-11 23:42:41 +0800

    [Zhu He] complete practice 3

--- src/main/java/com/thoughtworks/capability/gtb/MeetingSystemV3.java
+++ src/main/java/com/thoughtworks/capability/gtb/MeetingSystemV3.java
@@ -1,41 +1,54 @@
 package com.thoughtworks.capability.gtb;
 
-import java.time.LocalDateTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 
 /**
  * 脑洞会议系统v3.0
  * 1.当前会议时间"2020-04-01 14:30:00"表示伦敦的本地时间，而输出的新会议时间是芝加哥的本地时间
- *   场景：
- *   a:上个会议是伦敦的同事定的，他在界面上输入的时间是"2020-04-01 14:30:00"，所以我们要解析的字符串是伦敦的本地时间
- *   b:而我们在当前时区(北京时区)使用系统
- *   c:我们设置好新会议时间后，要发给芝加哥的同事查看，所以格式化后的新会议时间要求是芝加哥的本地时间
+ * 场景：
+ * a:上个会议是伦敦的同事定的，他在界面上输入的时间是"2020-04-01 14:30:00"，所以我们要解析的字符串是伦敦的本地时间
+ * b:而我们在当前时区(北京时区)使用系统
+ * c:我们设置好新会议时间后，要发给芝加哥的同事查看，所以格式化后的新会议时间要求是芝加哥的本地时间
  * 2.用Period来实现下个会议时间的计算
  *
  * @author itutry
- * @create 2020-05-19_18:43
+ * @author he.zhu
  */
 public class MeetingSystemV3 {
 
-  public static void main(String[] args) {
-    String timeStr = "2020-04-01 14:30:00";
+    public static void main(String[] args) {
+        String timeStr = "2020-04-01 14:30:00";
 
-    // 根据格式创建格式化类
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-    // 从字符串解析得到会议时间
-    LocalDateTime meetingTime = LocalDateTime.parse(timeStr, formatter);
+        // 根据格式创建格式化类
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        // 从字符串解析得到会议时间
+        LocalDateTime meetingTime = LocalDateTime.parse(timeStr, formatter);
+        // 设置会议时间zone为London
+        ZonedDateTime LondonDateTime = ZonedDateTime.of(meetingTime, ZoneId.of("Europe/London"));
 
-    LocalDateTime now = LocalDateTime.now();
-    if (now.isAfter(meetingTime)) {
-      LocalDateTime tomorrow = now.plusDays(1);
-      int newDayOfYear = tomorrow.getDayOfYear();
-      meetingTime = meetingTime.withDayOfYear(newDayOfYear);
+        ZonedDateTime now = ZonedDateTime.now(ZoneId.of("Europe/London"));
 
-      // 格式化新会议时间
-      String showTimeStr = formatter.format(meetingTime);
-      System.out.println(showTimeStr);
-    } else {
-      System.out.println("会议还没开始呢");
+        Period period;
+
+        if (now.isAfter(LondonDateTime)) {
+            period = Period.between(meetingTime.toLocalDate(), LocalDate.now());
+            period = Period.between(meetingTime.toLocalDate(), LocalDate.now());
+            period = period.plusDays(1);
+
+            meetingTime = meetingTime.plus(period);
+
+            // 转化成zoned date time
+            LondonDateTime = ZonedDateTime.of(meetingTime, ZoneId.of("Europe/London"));
+
+            // 转化成芝加哥时间
+            ZonedDateTime ChicagoDateTime = LondonDateTime.withZoneSameInstant(ZoneId.of("America/Chicago"));
+            String showTimeStr = formatter.format(ChicagoDateTime);
+
+            // 打印芝加哥下次会议的时间
+            System.out.println(showTimeStr);
+        } else {
+            System.out.println("会议还没开始呢");
+        }
     }
-  }
 }
